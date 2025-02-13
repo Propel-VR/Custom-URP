e20b49cc4284d67e8d6c5c615b8c6b2c05fb5036
Propel-antonin <antonin@propelvr.ca>
2025-02-13 12:20:31 -0500

    Update UPDATE_UPSTREAM_BRANCH.bat

--- UPDATE_UPSTREAM_BRANCH.bat
+++ UPDATE_UPSTREAM_BRANCH.bat
@@ -3,31 +3,31 @@ cd /d %~dp0
 
 REM Variables
 set UPSTREAM_REPO=https://github.com/Unity-Technologies/Graphics
-set FORK_REPO=https://github.com/Propel-VR/Custom-URP
 set SUBFOLDER=Packages/com.unity.render-pipelines.universal
-set UPSTREAM_BRANCH=upstream_master
+set NEW_BRANCH=incoming_changes
 
-REM Clone your fork locally
-git clone %FORK_REPO% .
-cd %~dp0
-
-REM Add the original repository as a remote
-git remote add upstream %UPSTREAM_REPO%
+REM Check if the 'upstream' remote already exists
+git remote | findstr upstream
+if %errorlevel%==1 (
+    REM Add the original repository as a remote
+    git remote add upstream %UPSTREAM_REPO%
+)
 
 REM Fetch only the master branch from the original repository
 git fetch upstream master
 
-REM Checkout the specific subfolder from the original repository
-git checkout -b %UPSTREAM_BRANCH% upstream/master -- %SUBFOLDER%
+REM Checkout a new branch for the incoming changes
+git checkout -b %NEW_BRANCH%
 
-REM Merge the changes into your local fork
-git merge %UPSTREAM_BRANCH%
+REM Ensure we are in the correct directory for merging
+cd /d %~dp0
 
-REM Delete all branches except for master
-for /f "tokens=*" %%i in ('git branch ^| findstr /v "master"') do git branch -D %%i
+REM Merge the contents of the subfolder from upstream master into the root directory
+git checkout upstream/master -- %SUBFOLDER%/
+git mv %SUBFOLDER%/* .
 
-REM Resolve conflicts manually if needed, then commit the changes
-git commit -m "Merged changes from upstream"
+REM Inform the user to resolve conflicts manually if needed and commit the changes
+echo Resolve conflicts manually if needed, then commit the changes
+echo After resolving conflicts, run: git commit -m "Merged changes from upstream"
 
-REM Push the changes to your GitHub repository
-git push origin master
+pause
